96cb2cec2ec8cb4849d222ad3ca7fba25d9bfa54
Manumac86 <emmartinez86@gmail.com>
2020-10-06 22:06:16 -0300

    Adding .env

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,13 +2,13 @@ import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyAqbDFNsDkcWtLWwxdEKxhhr0Al7Njwk6Q",
-  authDomain: "shoply-coder.firebaseapp.com",
-  databaseURL: "https://shoply-coder.firebaseio.com",
-  projectId: "shoply-coder",
-  storageBucket: "shoply-coder.appspot.com",
-  messagingSenderId: "336595987075",
-  appId: "1:336595987075:web:a44364b1a83bacff6c49b6"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 });
 
 export function getFirebase() {
